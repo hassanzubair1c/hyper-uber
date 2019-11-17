2cf6d13c7c20444ccd9c2bf14782f176b5bfab5c
Hassan XBOT <hassanxbot1c@gmail.com>
2019-11-17 12:06:27 +0500

    First Commit to Test

---
+++ Hyper Uber/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace Hyper_Uber
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ Hyper Uber/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Hyper_Uber.Startup))]
+namespace Hyper_Uber
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
